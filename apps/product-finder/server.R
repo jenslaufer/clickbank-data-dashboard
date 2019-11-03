236ee94ea457ccf95187f526bd196cf54c0f8634
jenslaufer <jenslaufer@gmail.com>
2019-11-03 15:46:46 +0100

    added all more filters

--- apps/product-finder/server.R
+++ apps/product-finder/server.R
@@ -6,24 +6,40 @@ source("../api.R")
 
 .filter.values <- function(data, input) {
     data %>%
-        filter(Gravity >= input$gravity[1] &
-                   Gravity <= input$gravity[2]) %>%
+        filter(Gravity >= input$Gravity[1] &
+                   Gravity <= input$Gravity[2]) %>%
         filter(
-            AverageEarningsPerSale >= input$average.earnings[1] &
-                AverageEarningsPerSale <= input$average.earnings[2]
+            AverageEarningsPerSale >= input$AverageEarningsPerSale[1] &
+                AverageEarningsPerSale <= input$AverageEarningsPerSale[2]
         ) %>%
         filter(
-            InitialEarningsPerSale >= input$initial.earnings[1] &
-                InitialEarningsPerSale <= input$initial.earnings[2]
-        )
+            InitialEarningsPerSale >= input$InitialEarningsPerSale[1] &
+                InitialEarningsPerSale <= input$InitialEarningsPerSale[2]
+        ) %>%
+        filter(
+            PercentPerRebill >= input$PercentPerRebill[1] &
+                PercentPerRebill <= input$PercentPerRebill[2]
+        ) %>%
+        filter(
+            PercentPerSale >= input$PercentPerSale[1] &
+                PercentPerSale <= input$PercentPerSale[2]
+        ) %>%
+        filter(
+            TotalRebillAmt >= input$TotalRebillAmt[1] &
+                TotalRebillAmt <= input$TotalRebillAmt[2]
+        ) %>%
+        filter(Referred >= input$Referred[1] &
+                   Referred <= input$Referred[2]) %>%
+        filter(Commission >= input$Commission[1] &
+                   Commission <= input$Commission[2])
 }
 
-.update.slider <- function(data, session, name, field) {
+.update.slider <- function(data, session, field) {
     max <- data %>% pull(field) %>% max()
     min <- data %>% pull(field) %>% min()
     updateSliderInput(
         session,
-        name,
+        field,
         max = max,
         min = min,
         value = c(min, max)
@@ -35,16 +51,14 @@ shinyServer(function(input, output, session) {
     
     data <- load.data()
     
-    .update.slider(data, session, "gravity", "Gravity")
-    .update.slider(data,
-                   session,
-                   "average.earnings",
-                   "AverageEarningsPerSale")
-    .update.slider(data,
-                   session,
-                   "initial.earnings",
-                   "InitialEarningsPerSale")
-    .update.slider(data, session, "percent.per.rebill", "PercentPerRebill")
+    .update.slider(data, session, "Gravity")
+    .update.slider(data, session, "AverageEarningsPerSale")
+    .update.slider(data, session, "InitialEarningsPerSale")
+    .update.slider(data, session, "PercentPerRebill")
+    .update.slider(data, session, "PercentPerSale")
+    .update.slider(data, session, "TotalRebillAmt")
+    .update.slider(data, session, "Referred")
+    .update.slider(data, session, "Commission")
     
     loginfo("initializing.")
     
--- apps/product-finder/ui.R
+++ apps/product-finder/ui.R
@@ -1,31 +1,27 @@
 library(shiny)
 
+
+.slider.input <- function(name, title) {
+    sliderInput(name,
+                title,
+                min = 0,
+                max = 100,
+                value = c(0, 100))
+}
+
 shinyUI(fluidPage(
     titlePanel("Clickbank Niche Finder"),
     
     sidebarLayout(
         sidebarPanel(
-            sliderInput(
-                "gravity",
-                "Gravity",
-                min = 0,
-                max = 1000,
-                value = c(0, 1000)
-            ),
-            sliderInput(
-                "average.earnings",
-                "Average Earnings Per Sale",
-                min = 0,
-                max = 100,
-                value = c(0, 100)
-            ),
-            sliderInput(
-                "initial.earnings",
-                "Initial Earnings Per Sale",
-                min = 0,
-                max = 100,
-                value = c(0, 100)
-            )
+            .slider.input("Gravity", "Gravity"),
+            .slider.input("AverageEarningsPerSale", "Average Earnings Per Sale"),
+            .slider.input("InitialEarningsPerSale", "Initial Earnings Per Sale"),
+            .slider.input("PercentPerRebill", "Percent Per Rebill"),
+            .slider.input("PercentPerSale", "Percent Per Sale"),
+            .slider.input("TotalRebillAmt", "Total Rebill Amt"),
+            .slider.input("Referred", "Referred") ,
+            .slider.input("Commission", "Commission")
         ),
         
         mainPanel(plotOutput("gravityPlot"), DT::dataTableOutput("products"))
